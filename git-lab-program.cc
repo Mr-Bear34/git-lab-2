ef26b806da8eb80a740e18ab0e640e8c47cf8fd6
Ryan <rw089716@ohio.edu>
2019-09-06 16:41:59 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Ryan Walsh
+ *      Date: 09/06/2019
+ *  Description: To practice using github and uploading files onto the site.
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *argv[]){
+   
+     cout << "Hello Git!" << endl;
+     /*add code*/
+     return 0;
+ }// main
